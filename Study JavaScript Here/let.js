11f86e0369b2e1da4aa13ff737282724ccc7b668
Maunducyrus <maunducyrus123@gmail.com>
2025-03-11 21:42:49 +0300

    for

--- Study JavaScript Here/let.js
+++ Study JavaScript Here/let.js
@@ -1,8 +1,15 @@
-let x = 5;
-console.log(x);
+// let x = 5;
+// console.log(x);
 
-var y = 10;
-console.log(y);
+// var y = 10;
+// console.log(y);
 
-var z = 12;
-console.log(z);
+// var z = 12;
+// console.log(z);
+
+function count() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
